a9a048e2a8b32e5cc9f45eb9f6a9d36163fe160d
Scttpr <charles.capelli@qonfucius.team>
2021-05-01 22:33:06 +0200

    chore: replace tailwindCSS by windiCSS

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,12 @@
+// @todo resolve issue with virtual imports
+/* eslint-disable import/no-unresolved */
 import { createApp } from 'vue';
 
 import App from './App.vue';
 import router from './router';
+
+import 'virtual:windi.css';
+import 'virtual:windi-devtools';
 
 import './assets/css/index.css';
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite';
 
-import vue from '@vitejs/plugin-vue';
-import pages from 'vite-plugin-pages';
+import Vue from '@vitejs/plugin-vue';
+import Pages from 'vite-plugin-pages';
+import WindiCSS from 'vite-plugin-windicss'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue(), pages()],
+    plugins: [Vue(), Pages(), WindiCSS()],
 });
