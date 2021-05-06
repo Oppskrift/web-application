310ca79ec68f9bc7dfc1dddb9de117ea8654a92d
Scttpr <charles.capelli@qonfucius.team>
2021-05-06 19:30:07 +0200

    test: added basic test to homepage

---
+++ src/pages/index.spec.ts
@@ -0,0 +1,13 @@
+import { mount } from '@vue/test-utils';
+
+import HomePage from './index.vue';
+
+describe('Icon Button', () => {
+    test('It should pass this dummy test', async () => {
+        const wrapper = mount(HomePage);
+
+        const button = wrapper.find('main');
+
+        expect(button.exists()).toBe(true);
+    });
+});
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,14 +1,11 @@
 <template>
-    <main><Button label="Home page button" /></main>
+    <main>Hello world</main>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 
-import Button from '../components/Button';
-
 export default defineComponent({
     name: 'HomePage',
-    components: { Button },
 });
 </script>
