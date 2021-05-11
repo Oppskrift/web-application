d7a6c1f46f8573b9469c541d98eb6d03d27ec56d
Scttpr <charles.capelli@qonfucius.team>
2021-05-11 20:37:39 +0200

    feat: ingredient component

---
+++ src/components/molecules/Ingredient/Ingredient.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="text-sm flex items-center">
+        <img
+            class="w-8 h-8 object-cover mr-4"
+            v-if="image.path"
+            :src="image.path"
+            :alt="image.alternativeText"
+        />
+        <div>
+            <p>
+                <strong>{{ displayableQuantity }}</strong>
+            </p>
+            <p>{{ name }}</p>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { computed, defineComponent, PropType } from 'vue';
+
+interface Image {
+    path: string;
+    alternativeText: string;
+}
+
+interface Quantity {
+    value: number;
+    unit: string;
+}
+
+export default defineComponent({
+    name: 'Ingredient',
+    props: {
+        name: {
+            type: String,
+            required: true,
+        },
+        quantity: {
+            type: Object as PropType<Quantity>,
+            required: true,
+        },
+        image: {
+            type: Object as PropType<Image>,
+            default: () => ({}),
+        },
+    },
+    setup(props) {
+        const displayableQuantity = computed(
+            () => `${props.quantity.value} ${props.quantity.unit}`
+        );
+
+        return { displayableQuantity };
+    },
+});
+</script>
+
+<style scoped></style>
